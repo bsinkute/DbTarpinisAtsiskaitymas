7d25e905f987d943ffc2a498ae4ba88cbe311722
Bronislava <sinkute.bronislava@gmail.com>
2024-05-20 20:13:38 +0300

    Added lectures when creating department to all added students

--- DbTarpinisAtsiskaitymas/Windows/CreateDepartmentAndAddStudentAndLectureWindow.cs
+++ DbTarpinisAtsiskaitymas/Windows/CreateDepartmentAndAddStudentAndLectureWindow.cs
@@ -1,5 +1,6 @@
 ﻿using DbTarpinisAtsiskaitymas.Helpers;
 using DbTarpinisAtsiskaitymas.Interfaces;
+using DbTarpinisAtsiskaitymas.Models;
 
 namespace DbTarpinisAtsiskaitymas.Windows
 {
@@ -17,6 +18,7 @@ namespace DbTarpinisAtsiskaitymas.Windows
         }
         public async Task CreateDepartmentAndAddStudentAndLecture()
         {
+            List<Student> addedStudents = new List<Student>();
             Console.Clear();
             Console.WriteLine("Creating a new department!");
             Console.Write("Enter department name: ");
@@ -27,7 +29,8 @@ namespace DbTarpinisAtsiskaitymas.Windows
             bool addMoreStudents;
             do
             {
-                await ConsoleHelper.CreateStudent(department.DepartmentId, _studentService);
+                var student = await ConsoleHelper.CreateStudent(department.DepartmentId, _studentService);
+                addedStudents.Add(student);
 
                 Console.Write("Do you want to add another student? (yes/no): ");
                 string response = Console.ReadLine().Trim().ToLower();
@@ -37,6 +40,7 @@ namespace DbTarpinisAtsiskaitymas.Windows
             Console.WriteLine("Assignment of new lectures!");
             var lectures = await _lectureService.GetAllLectures();
             bool addMoreLectures;
+            List<int> addedLectureIds = new List<int>();
             do
             {
                 if (!lectures.Any())
@@ -50,6 +54,7 @@ namespace DbTarpinisAtsiskaitymas.Windows
                     .ToList();
                 var lectureId = ConsoleHelper.SelectLectureId(lecturesNotInDepartment);
                 await _lectureService.AddLectureDepartment(lectureId, department.DepartmentId);
+                addedLectureIds.Add(lectureId);
                 var lecture = lectures.FirstOrDefault(l => l.LectureId == lectureId);
                 Console.WriteLine($"Lecture '{lecture.LectureName}' has been added to department ID '{department.DepartmentId}'.");
 
@@ -58,6 +63,15 @@ namespace DbTarpinisAtsiskaitymas.Windows
                 addMoreLectures = response == "yes";
 
             } while (addMoreLectures);
+
+            foreach (var student in addedStudents)
+            {
+                if (addedLectureIds.Count == 0)
+                {
+                    break;
+                }
+                await _lectureService.AssignLecturesToStudent(student.StudentId, addedLectureIds);
+            }
 
             Console.WriteLine("Finished adding lectures.");
             Console.WriteLine($"You added a department {department.DepartmentName} with students and lectures.");
